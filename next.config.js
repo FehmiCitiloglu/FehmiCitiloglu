ac15e8178102efbd4d498dcdc2c86223bb425657
FehmiCitiloglu <fehmicitiloglu@gmail.com>
2023-09-12 01:03:29 +0300

    fix next.config file

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    reactStrictMode: true,
+    basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+    assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+    images: {
+      loader: "imgix",
+      path: "http://www.fehmicitiloglu.tech/",
+    },
+}
 
 module.exports = nextConfig
